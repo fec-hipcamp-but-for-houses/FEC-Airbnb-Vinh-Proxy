4de2591e6c55db5c7697c15a176e6f3171ac2e78
Vinh Tran <vincen7.tran@gmail.com>
2019-05-30 21:12:29 -0700

    Refactored out Webpack using CDNs

--- server.js
+++ server.js
@@ -1,18 +1,11 @@
 const express = require('express');
 const path = require('path');
-const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 3000;
 const publicDir = path.join(__dirname, './public');
 
 app.use(express.static(publicDir));
-
-app.get('/listing', (req, res) => {
-  const { listingId } = req.query;
-  const queryString = `/listing?listingId=${listingId}`;
-  axios(`http://localhost:3002/${queryString}`);
-})
 
 app.listen(port, () => console.log('Listening on Port:', port));
 
